8092ffde7b6119943766c64dc1cf004eb909bc7f
utkrshydv <utkarsh4socials@gmail.com>
2026-05-11 21:29:10 +0530

    setup pydantic and schemas

--- fastapi_blog/main.py
+++ fastapi_blog/main.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 
 
 
@@ -48,11 +49,28 @@ def post_page(request: Request, post_id: int):
             )
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.post(
+        "/api/posts",
+        response_model=PostResponse,
+        status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts ) + 1 if posts else 1
+    new_post ={
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "May 11, 2026"
+    }
+    posts.append(new_post)
+    return new_post  
+
+@app.get("/api/posts/{post_id}", response_model = PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
